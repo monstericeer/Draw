7c003786dc269c7de68b0ea2c61d43f454da7bb3
PYL <1874157401@qq.com>
2017-06-13 10:55:38 +0800

    add color
    add color

--- js/color.js
+++ js/color.js
@@ -6,10 +6,58 @@ var lineColor_arr = [
 	'rgb(201, 74, 83)',		//红
 	'rgb(101, 56, 125)',	//紫
 	'rgb(233, 215, 142)',	//黄
-	'rgb(86, 152, 196)'		//蓝
+	'rgb(86, 152, 196)',	//蓝
+	'rgb(252,146,59)'		//橘
 ];
 
 // 默认线条颜色
-var line_color = lineColor_arr[6];
+var line_color = lineColor_arr[0];
+
+$('.colorBox').on('touchstart',function(){
+	$('.s0').css('opacity','0.8');
+})
+
+var colorTog = true;
+$('.colorBox').on('touchend',function(){
+	$('.c0').css('opacity','1');
+	
+	if(colorTog){
+		colorTog = !colorTog;
+		colorOut();
+		sizeIn();
+	}else{
+		colorTog = !colorTog;
+		colorIn()
+	}
+});
 
 
+for(var i=1;i<=lineColor_arr.length;i++){
+	(function(j){
+		$('.c'+j).on('touchend',function(){
+			line_color = lineColor_arr[j-1];
+		});
+	})(i)
+}
+
+
+function colorOut(){
+	$('.c1').stop().animate({'top':'-4rem','left':'0rem','opacity': '1'},100)
+	$('.c2').stop().animate({'top':'-3rem','left':'3rem','opacity': '1'},200)
+	$('.c3').stop().animate({'top':'0rem','left':'4rem','opacity': '1'},300)
+	$('.c4').stop().animate({'top':'3rem','left':'3rem','opacity': '1'},400)
+	$('.c5').stop().animate({'top':'4rem','left':'0rem','opacity': '1'},500)
+	$('.c6').stop().animate({'top':'3rem','left':'-3rem','opacity': '1'},600)
+	$('.c7').stop().animate({'top':'0rem','left':'-4rem','opacity': '1'},700)
+	$('.c8').stop().animate({'top':'-3rem','left':'-3rem','opacity': '1'},800)
+}
+function colorIn(){
+	$('.c1').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},800)
+	$('.c2').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},700)
+	$('.c3').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},600)
+	$('.c4').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},500)
+	$('.c5').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},400)
+	$('.c6').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},300)
+	$('.c7').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},200)
+	$('.c8').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},100)
+}
--- js/size.js
+++ js/size.js
@@ -4,40 +4,43 @@ var lineWidth_arr = [1,4,8,12];
 // 默认线条宽度
 var line_width = lineWidth_arr[0];
 
-var sizeBox = document.querySelector(".sizeBox");
 
-
-sizeBox.addEventListener('touchstart',sizeBoxTouchStart,false);
-sizeBox.addEventListener('touchend',sizeBoxTouchEnd,false);
-
-function sizeBoxTouchStart() {
-	$('.s0').css('opacity','0.9');
-}
+$('.sizeBox').on('touchstart',function(){
+	$('.s0').css('opacity','0.8');
+})
 
 var sizeTog = true;
-function sizeBoxTouchEnd() {
+$('.sizeBox').on('touchend',function(){
 	$('.s0').css('opacity','1');
 	
 	if(sizeTog){
 		sizeTog = !sizeTog;
-		$('.s1').stop().animate({'top':'-4rem','left':'0rem','opacity': '1'},100)
-		$('.s2').stop().animate({'top':'-3rem','left':'3rem','opacity': '1'},200)
-		$('.s3').stop().animate({'top':'0rem','left':'4rem','opacity': '1'},300)
-		$('.s4').stop().animate({'top':'3rem','left':'3rem','opacity': '1'},400)
+		sizeOut();
+		colorIn()
 	}else{
 		sizeTog = !sizeTog;
-		$('.s1').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},400)
-		$('.s2').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},300)
-		$('.s3').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},200)
-		$('.s4').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},100)
+		sizeIn()
 	}
-}
+});
+
 
 for(var i=1;i<=lineWidth_arr.length;i++){
 	(function(j){
 		$('.s'+j).on('touchend',function(){
 			line_width = lineWidth_arr[j-1];
-			console.log(line_width)
 		});
 	})(i)
+}
+
+function sizeOut(){
+	$('.s1').stop().animate({'top':'-4rem','left':'0rem','opacity': '1'},100)
+	$('.s2').stop().animate({'top':'-3rem','left':'3rem','opacity': '1'},230)
+	$('.s3').stop().animate({'top':'0rem','left':'4rem','opacity': '1'},330)
+	$('.s4').stop().animate({'top':'3rem','left':'3rem','opacity': '1'},400)
+}
+function sizeIn(){
+	$('.s1').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},400)
+	$('.s2').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},300)
+	$('.s3').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},200)
+	$('.s4').stop().animate({'top':'0rem','left':'0rem','opacity': '0'},100)
 }
